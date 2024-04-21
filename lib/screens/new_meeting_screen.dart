84001da021d0bc562e2c5778ad702c412123d426
czarbasanal <czarbasanal@gmail.com>
2024-04-21 23:07:16 +0800

    added cupertino switch

--- lib/screens/new_meeting_screen.dart
+++ lib/screens/new_meeting_screen.dart
@@ -1,7 +1,16 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class NewMeetingScreen extends StatelessWidget {
+class NewMeetingScreen extends StatefulWidget {
   const NewMeetingScreen({super.key});
+
+  @override
+  _NewMeetingScreenState createState() => _NewMeetingScreenState();
+}
+
+class _NewMeetingScreenState extends State<NewMeetingScreen> {
+  bool _isVideoOn = false;
+  bool _isPMI = false;
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +43,59 @@ class NewMeetingScreen extends StatelessWidget {
         leadingWidth: 85,
       ),
       body: Column(
-        children: [],
+        children: [
+          const SizedBox(height: 24),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.15),
+                  spreadRadius: 0.75,
+                  blurRadius: 0.5,
+                  offset: const Offset(1, 0.25),
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                ListTile(
+                  title: const Text('Video on'),
+                  trailing: CupertinoSwitch(
+                    value: _isVideoOn,
+                    onChanged: (bool value) {
+                      setState(() {
+                        _isVideoOn = value;
+                      });
+                    },
+                    activeColor: CupertinoColors.activeGreen,
+                  ),
+                  tileColor: Colors.white,
+                ),
+                Divider(
+                  height: 0,
+                  indent: 17,
+                  color: Colors.grey.withOpacity(0.2),
+                ),
+                ListTile(
+                  title: const Text('Use personal meeting ID (PMI)'),
+                  subtitle: const Text('286 541 3061'),
+                  subtitleTextStyle: const TextStyle(color: Color(0xFF6E6E6E)),
+                  trailing: CupertinoSwitch(
+                    value: _isPMI,
+                    onChanged: (bool value) {
+                      setState(() {
+                        _isPMI = value;
+                      });
+                    },
+                    activeColor: CupertinoColors.activeGreen,
+                  ),
+                  tileColor: Colors.white,
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
